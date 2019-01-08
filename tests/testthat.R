7a17b4a3954f023b48da2fc63f649ed2fadbda98
Niyazu <n.madin@campus.lmu.de>
2019-01-08 19:49:07 +0100

    Update testthat.R

--- tests/testthat.R
+++ tests/testthat.R
@@ -6,11 +6,10 @@ test_file("get_plot_data.R")
 context("valid data frame")
 test_that("valid types in data frame", {
   expect_is(plot_data, "data.frame")
-  expect_is(plot_data$x, "")
+  expect_is(plot_data$x, "numeric")
 })
 
 test_that("data types correct", {
   expect_is(plot_data,"data.frame")
-  expect_is(plot_data$numbers, "integer")
 })
 
