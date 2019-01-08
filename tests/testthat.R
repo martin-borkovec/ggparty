03e45df42e8c2ddf7892c11bb639779b3e8e9c73
Niyazu <n.madin@campus.lmu.de>
2019-01-08 14:33:27 +0100

    Tests directory added

--- tests/testthat.R
+++ tests/testthat.R
@@ -2,3 +2,15 @@ library(testthat)
 library(ggparty)
 
 test_check("ggparty")
+test_file("get_plot_data.R")
+context("valid data frame")
+test_that("valid types in data frame", {
+  expect_is(plot_data, "data.frame")
+  expect_is(plot_data$x, "")
+})
+
+test_that("data types correct", {
+  expect_is(plot_data,"data.frame")
+  expect_is(plot_data$numbers, "integer")
+})
+
