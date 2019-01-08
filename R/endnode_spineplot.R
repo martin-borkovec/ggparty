78507de0f523ccb6b1104857b7622808fbb062be
unknown <martin.borkovec@skyforge.at>
2019-01-08 15:54:02 +0100

    fixed shared legend

--- R/endnode_spineplot.R
+++ R/endnode_spineplot.R
@@ -10,21 +10,25 @@ endnode_spineplot <- function(party, gglist = NULL, shared_legend = T, ...) {
     plot_list[i] <- get_terminal_plot(party[[1]], party[[id]], gglist, ...)
   }
 
-  grob <- arrangeGrob(grobs = plot_list,
-                           nrow = 1)
 
   if (shared_legend == T) {
     g <- ggplotGrob(plot_list[[1]] + theme(legend.position = "bottom"))$grobs
     legend <- g[[which(sapply(g, function(x)
       x$name) == "guide-box")]]
-    lheight <- sum(legend$height)
-    lwidth <- sum(legend$width)
+    # lheight <- sum(legend$height)
+    # lwidth <- sum(legend$width)
     plot_list <- lapply(plot_list, function(x)
     x + theme(legend.position = "none"))
+    grob <- arrangeGrob(grobs = plot_list,
+                        nrow = 1)
     grob <- arrangeGrob(grob, legend,
                         nrow = 2,
                         heights = c(0.5, 0.1))
+  } else {
+    grob <- arrangeGrob(grobs = plot_list,
+                        nrow = 1)
   }
+
   annotation_custom(grob = grob, xmin = 0, xmax = 1, ymin = -0.5, ymax = 0)
 }
 
