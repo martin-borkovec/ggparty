036aa50f72d1575614aff9bc20e946239b928d5f
Martin <martin.borkovec@skyforge.at>
2019-01-22 19:28:47 +0100

    shared legend

--- R/geom_nodeplot.R
+++ R/geom_nodeplot.R
@@ -16,6 +16,7 @@ geom_nodeplot <- function(mapping = NULL,
                           gglist = NULL,
                           plot_call = NULL,
                           same_axes_limits = TRUE,
+                          shared_legend = FALSE,
                           ...) {
   layer(
     data = data,
@@ -30,6 +31,7 @@ geom_nodeplot <- function(mapping = NULL,
       gglist = gglist,
       plot_call = enquote(plot_call),
       same_axes_limits = same_axes_limits,
+      shared_legend = shared_legend,
       ...
     )
   )
@@ -46,55 +48,91 @@ GeomNodeplot <- ggproto(
                         na.rm = FALSE,
                         gglist,
                         plot_call,
-                        same_axes_limits) {
+                        same_axes_limits,
+                        shared_legend) {
 
     data <- coord$transform(data, panel_params)
+
+    # same axes limits --------------------------------------------------------
+    # generate ggplot object
+    x <- list()
+    x$node_data <- data[data$id == 1, , drop = FALSE]
+    base_plot <- eval(plot_call) + gglist
+
     if (same_axes_limits) {
-      x <- list()
-      x$node_data <- data[data$id == 1, , drop = FALSE]
-      g <- eval(plot_call) +
-        gglist
-      xlim <- ggplot_build(g)$layout$panel_params[[1]]$x.range
-      ylim <- ggplot_build(g)$layout$panel_params[[1]]$y.range
+      xlim <- ggplot_build(base_plot)$layout$panel_params[[1]]$x.range
+      ylim <- ggplot_build(base_plot)$layout$panel_params[[1]]$y.range
       gglist <- c(gglist,
                   list(xlim(xlim),
                        ylim(ylim)))
     }
 
-    grobs <- lapply(1:max(data$id), function(i) {
-      node_data <- data[data$id == i, , drop = FALSE]
+    if (shared_legend) {
+      base_grob <- ggplotGrob(base_plot)$grobs
 
+      legend <- base_grob[[which(sapply(base_grob, function(x) x$name) == "guide-box")]]
+      legend$vp <- viewport(x = 0.1, y = 0.9, width = 1, height = 1)
+
+
+      base_data  <- layer_data(base_plot)
+      cols <- base_data[["colour"]]
+      names(cols) <- rownames(base_data)
+
+      gglist <- c(gglist,
+                  list(theme(legend.position="none"),
+                       quote(scale_color_manual(values = unique(x$cols[names(x$cols) %in%
+                                                                         rownames(x$node_data)])))
+                  )
+      )
+
+      # lheight <- sum(legend$height)
+      # lwidth <- sum(legend$width)
+    }
+
+    grobs <- lapply(1:max(data$id), function(i) {
+
+      node_data <- data[data$id == i, ]
       nodeplotGrob(
         x = node_data$x[1],
         y = node_data$y[1],
         node_data = node_data,
         gglist = gglist,
-        plot_call = plot_call
+        plot_call = plot_call,
+        cols = cols
       )
     })
+
+    grobs <- c(grobs, list(legend))
     class(grobs) <- "gList"
     ggname("geom_nodeplot", grobTree(children = grobs))
   }
 )
 
-nodeplotGrob <- function(x, y, node_data, gglist, plot_call) {
+nodeplotGrob <- function(x, y, node_data, gglist, plot_call, cols) {
+
   gTree(x = x,
         y = y,
         node_data = node_data,
         plot_call = plot_call,
         gglist = gglist,
+        cols = cols,
         cl = "nodeplotgrob")
 }
 
 
 makeContent.nodeplotgrob <- function(x) {
+  browser()
   r <- ggplotGrob(
       eval(x$plot_call) +
-      x$gglist
+      x$gglist[1:4] + eval(x$gglist[[5]])
   )
 
   r$vp <- viewport(x = x$x, y = x$y, width = 0.1, height = 0.1)
   setChildren(x, gList(r))
 }
 
-
+# eval(x$plot_call) +
+#   geom_point(aes(colour = play, size = temperature)) +
+#   scale_color_manual(values = unique(x$cols[names(x$cols) %in%
+#                                               rownames(x$node_data)]))
+#   scale_color_manual(values = c("red", "blue"))
