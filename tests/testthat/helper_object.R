da0bc3537bbd8834b2663c653f19b86b6eba9f97
Niyazu <n.madin@campus.lmu.de>
2019-01-27 14:02:12 +0100

    Edit description and tests

---
+++ tests/testthat/helper_object.R
@@ -0,0 +1,20 @@
+data("WeatherPlay", package = "partykit")
+### splits ###
+## split in overcast, humidity, and windy
+sp_o <- partykit::partysplit(1L, index = 1:3)
+sp_h <- partykit::partysplit(3L, breaks = 75)
+sp_w <- partykit::partysplit(4L, index = 1:2)
+## query labels
+partykit::character_split(sp_o)
+### nodes ###
+## set up partynode structure
+pn <- partykit::partynode(1L, split = sp_o, kids = list(
+  partykit::partynode(2L, split = sp_h, kids = list(
+    partykit::partynode(3L, info = "yes"),
+    partykit::partynode(4L, info = "no"))),
+  partykit::partynode(5L, info = "yes"),
+  partykit::partynode(6L, split = sp_w, kids = list(
+    partykit::partynode(7L, info = "yes"),
+    partykit::partynode(8L, info = "no")))))
+py <- partykit::party(pn, WeatherPlay)
+
--- tests/testthat/test_structure.R
+++ tests/testthat/test_structure.R
@@ -1,38 +1,13 @@
-library(testthat)
-library(roxygen2)
-source("R/get_plot_data.R")
-source("R/ggparty.R")
-source("R/geom_node_terminal_plot.R")
-
 test_that("valid types in data frame", {
   expect_is(get_plot_data(party(pn, WeatherPlay)), "data.frame")
   expect_is(get_plot_data(party(pn, WeatherPlay))$x, "numeric")
   expect_is(get_plot_data(party(pn, WeatherPlay))$y, "numeric")
   expect_is(get_plot_data(party(pn, WeatherPlay))$parent, "integer")
-
 })
 
 test_that("valid structure", {
-  data("WeatherPlay", package = "partykit")
-
-  sp_o <- partysplit(1L, index = 1:3)
-  sp_h <- partysplit(3L, breaks = c(60,80))
-  sp_w <- partysplit(4L, index = 1:2)
-
-  character_split(sp_o)
-  pn <- partynode(1L, split = sp_o, kids = list(
-    partynode(2L, split = sp_h, kids = list(
-      partynode(3L, info = "yes"),
-      partynode(4L, info = "no"),
-      partynode(9L, info = "maybe"))),
-    partynode(5L, info = "yes"),
-    partynode(6L, split = sp_w, kids = list(
-      partynode(7L, info = "yes"),
-      partynode(8L, info = "no")))))
-
-
   plot <- ggparty(party(pn, WeatherPlay))
   #vdiffr::expect_doppelganger("ggplot", plot)
   expect_is(plot,"ggplot")
   expect_is(party(pn, WeatherPlay),"party")
-})
+ })
