fb6c84d75189184c26f47c83248ecb9c3a93d316
Niyazu <n.madin@campus.lmu.de>
2019-03-07 10:19:25 +0100

    Create test_visuals.R

---
+++ tests/testthat/test_visuals.R
@@ -0,0 +1,18 @@
+context("visual unit tests")
+test_that("plots drawn correctly", {
+  vdiffr::expect_doppelganger("WeatherPlay Plot",
+                              ggparty(py, horizontal = F) +
+                                geom_edge() +
+                                geom_node_splitvar(ynudge = 0.0) +
+                                geom_edge_label(shift = 0.5) +
+                                geom_node_info() +
+                                geom_nodeplot(gglist = list(geom_point(aes(temperature,humidity,shape = play,col = humidity,size = temperature))),
+                                ids = "terminal",
+                                scales = "fixed",
+                                width = 0.15,
+                                height = 0.15,
+                                y_nudge = 0,
+                                x_nudge = 0
+                                ) +
+                                ylim(-0.3, 1))
+  })
