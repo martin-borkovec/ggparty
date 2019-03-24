f7cc60ff231adb4fb3fa8a7d37be9e9e56a14575
Martin <martin.borkovec@skyforge.at>
2019-03-24 21:13:40 +0100

    geom_node_label finished

--- R/geom_node_label.R
+++ R/geom_node_label.R
@@ -17,6 +17,8 @@ geom_node_label <- function(mapping = NULL,
                             label.padding = unit(0.25, "lines"),
                             label.r = unit(0.15, "lines"),
                             label.size = 0.25,
+                            label.col = NULL,
+                            label.fill = NULL,
                             na.rm = FALSE,
                             show.legend = NA,
                             inherit.aes = TRUE) {
@@ -43,6 +45,8 @@ geom_node_label <- function(mapping = NULL,
       label.padding = label.padding,
       label.r = label.r,
       label.size = label.size,
+      label.fill = label.fill,
+      label.col = label.col,
       na.rm = na.rm,
       line_gpar = line_gpar,
       ids = ids,
@@ -61,7 +65,7 @@ GeomNodeLabel <- ggproto("GeomNodeLabel", Geom,
 
                      default_aes = aes(
                        colour = "black", fill = "white", size = 3.88, angle = 0,
-                       hjust = 0.5, vjust = 0.5, alpha = NA, family = "", fontface = 1,
+                      alpha = NA, family = "", fontface = 1,
                        lineheight = 1.2
                      ),
 
@@ -70,9 +74,11 @@ GeomNodeLabel <- ggproto("GeomNodeLabel", Geom,
                                            label.padding = unit(0.25, "lines"),
                                            label.r = unit(0.15, "lines"),
                                            label.size = 0.25,
+                                           label.col = NULL,
+                                           label.fill = NULL,
                                            aes_list,
-                                           line_gpar) {
-                       #lab <- data$label
+                                           line_gpar
+                                           ) {
 
                        on.exit(detach(data))
                        attach(data)
@@ -81,33 +87,16 @@ GeomNodeLabel <- ggproto("GeomNodeLabel", Geom,
                          labs <- character()
                          for (i in seq_along(aes_list)) {
                            labs[i] <- rlang::eval_tidy(aes_list[[i]]$label)[j]
+                           if (i %in% parse) labs[i] <- ggplot2:::parse_safe(as.character(labs[i]))
 
                          }
                          lab[[j]] <- labs
-
                        }
-
-
-                       # if (parse) {
-                       #   lab <- parse_safe(as.character(lab))
-                       # }
 
                        data <- coord$transform(data, panel_params)
-                       if (is.character(data$vjust)) {
-                         data$vjust <- compute_just(data$vjust, data$y)
-                       }
-                       if (is.character(data$hjust)) {
-                         data$hjust <- compute_just(data$hjust, data$x)
-                       }
-
-
-
-
-
 
                        grobs <- lapply(1:nrow(data), function(i) {
                          row <- data[i, , drop = FALSE]
-                         #browser()
                          text_gp <- list()
                          for (j in seq_along(line_gpar)) {
 
@@ -135,23 +124,19 @@ GeomNodeLabel <- ggproto("GeomNodeLabel", Geom,
                            )))
                          }
 
-#browser()
-
                          nodelabelGrob(lab[[i]],
                                    x = unit(row$x, "native"),
                                    y = unit(row$y, "native"),
-                                   just = c(ifelse(is.null(line_gpar[[j]]$hjust),
-                                                   row$hjust,
-                                                   resolveHJust(line_gpar[[j]]$hjust, NULL)),
-                                            ifelse(is.null(line_gpar[[j]]$vjust),
-                                                   row$vjust,
-                                                   resolveVJust(line_gpar[[j]]$vjust, NULL))),
                                    padding = label.padding,
                                    r = label.r,
                                    text.gp = text_gp,
                                    rect.gp = gpar(
-                                     col = if (isTRUE(all.equal(label.size, 0))) NA else row$colour,
-                                     fill = alpha(row$fill, row$alpha),
+                                     col = ifelse(is.null(label.col),
+                                                  if (isTRUE(all.equal(label.size, 0))) NA else row$colour,
+                                                 label.col),
+                                     fill = ifelse(is.null(label.fill),
+                                                   alpha(row$fill, row$alpha),
+                                                   label.fill),
                                      lwd = label.size * .pt
                                    )
                          )
@@ -165,30 +150,27 @@ GeomNodeLabel <- ggproto("GeomNodeLabel", Geom,
 )
 
 nodelabelGrob <- function(label, x = unit(0.5, "npc"), y = unit(0.5, "npc"),
-                      just = "center", padding = unit(0.25, "lines"), r = unit(0.1, "snpc"),
+                     padding = unit(0.25, "lines"), r = unit(0.1, "snpc"),
                       default.units = "npc", name = NULL,
                       text.gp = gpar(), rect.gp = gpar(fill = "white"), vp = NULL) {
-
-  #stopifnot(length(label) == 1)
 
   if (!is.unit(x))
     x <- unit(x, default.units)
   if (!is.unit(y))
     y <- unit(y, default.units)
 
-  gTree(label = label, x = x, y = y, just = just, padding = padding, r = r,
+  gTree(label = label, x = x, y = y, padding = padding, r = r,
         name = name, text.gp = text.gp, rect.gp = rect.gp, vp = vp, cl = "nodelabelgrob")
 }
 
 #' @export
 makeContent.nodelabelgrob <- function(x) {
-  hj <- resolveHJust(x$just, NULL)
-  vj <- resolveVJust(x$just, NULL)
-
-  #browser()
 
   text_list <- list()
   for (i in seq_along(x$label)) {
+
+# get vector telling textgrob how many lines to move y --------------------
+
 
     y_shift <- rep(0, length(x$label))
     if(length(x$label) %% 2 == 0)
@@ -220,6 +202,10 @@ makeContent.nodelabelgrob <- function(x) {
         }
       }
 
+
+# get vector for rectgrob y shift -----------------------------------------
+
+
     if (i == 1) {
       box_up <- y_shift
       box_up[1] <- 1
@@ -231,13 +217,10 @@ makeContent.nodelabelgrob <- function(x) {
 
     text_list <- c(text_list, list(textGrob(
       label = x$label[i],
-      x = x$x + 2 * (0.5 - hj) * x$padding,
-      y = x$y + 2 * (0.5 - vj) * x$padding + unit(sum(y_shift *
+      x = x$x,
+      y = x$y +
+        unit(sum(y_shift *
         sapply(x$text.gp, function(x) x$fontsize * x$lineheight)), "point"),
-        #unit(x$text.gp[[i]]$fontsize * x$text.gp[[i]]$lineheight, "points"),
-
-
-      just = c(hj, vj),
       gp = x$text.gp[[i]],
       name = paste0("text", i))))
 
@@ -250,7 +233,6 @@ makeContent.nodelabelgrob <- function(x) {
 
   }
 
-#browser()
   r <- list(roundrectGrob(x = x$x,
                           y = x$y +
                           unit(
@@ -262,7 +244,6 @@ makeContent.nodelabelgrob <- function(x) {
                           width = max(widths) + 2 * x$padding,
                           height = unit(sum(sapply(x$text.gp, function(x)
                             x$fontsize * x$lineheight)), "point") +  2 * x$padding,
-                          just = c(hj, vj),
                           r = x$r,
                           gp = x$rect.gp,
                           name = "box"
@@ -271,6 +252,4 @@ makeContent.nodelabelgrob <- function(x) {
   grob_list <- c(r, text_list)
   class(grob_list) <- "gList"
   ggname("geom_nodelabels", grid::grobTree(children = grob_list))
-
-  #setChildren(x, gList(r, t1, t2))
 }
