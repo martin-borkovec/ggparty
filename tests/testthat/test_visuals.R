5248b09ae6421ad66e63d0e21c1ce0aaf954a121
Niyazu <n.madin@campus.lmu.de>
2019-03-27 16:29:30 +0100

    validated visual test

--- tests/testthat/test_visuals.R
+++ tests/testthat/test_visuals.R
@@ -1,19 +1,12 @@
 context("visual unit tests")
 test_that("plots drawn correctly", {
   vdiffr::expect_doppelganger("WeatherPlay Plot",
-                              ggparty(py, horizontal = F) +
+                              ggparty(py) +
                                 geom_edge() +
-                                geom_node_splitvar(nudge_y = 0.0) +
-                                geom_edge_label(shift = 0.5) +
-                                geom_node_info() +
-                                geom_node_plot(gglist = list(geom_point(aes(temperature,humidity,shape = play,col = humidity,size = temperature))),
-                                ids = "terminal",
-                                scales = "fixed",
-                                width = 0.15,
-                                height = 0.15,
-                                y_nudge = 0,
-                                x_nudge = 0
-                                ) +
-                                ylim(-0.3, 1))
+                                geom_edge_label() +
+                                geom_node_label(aes(label = splitvar),
+                                                ids = "inner") +
+                                geom_node_label(aes(label = info),
+                                                ids = "terminal")
+                              )
   })
-
